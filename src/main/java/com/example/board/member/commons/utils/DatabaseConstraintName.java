505c95bddfbe8d187f40feb93a8d65b9de123021
miau-x2 <joined260130@gmail.com>
2026-03-13 11:30:39 +0900

    chore: MemberProfile 엔티티 데이터베이스 제약 조건명 변경

--- src/main/java/com/example/board/member/commons/utils/DatabaseConstraintName.java
+++ src/main/java/com/example/board/member/commons/utils/DatabaseConstraintName.java
@@ -4,8 +4,8 @@ public final class DatabaseConstraintName {
     private DatabaseConstraintName() {}
 
     public static final class MemberProfile {
-        public static final String PK = "PRIMARY";
-        public static final String HANDLE = "uk_member_profile_handle";
-        public static final String NICKNAME = "uk_member_profile_nickname";
+        public static final String PK = "member_profile.PRIMARY";
+        public static final String HANDLE = "member_profile.uk_member_profile_handle";
+        public static final String NICKNAME = "member_profile.uk_member_profile_nickname";
     }
 }
